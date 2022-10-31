ec93179a35fd272744124d8fa4b6e791047407b2
Benjamin Dagg <bdagg@DiamondGame.com>
2022-10-31 08:28:32 -0700

    Changed SessionManager startup method to create desktop session

--- POS-Automation/SessionManager.cs
+++ POS-Automation/SessionManager.cs
@@ -6,6 +6,7 @@ using Appium;
 using OpenQA.Selenium.Appium;   //Appium Options
 using System.Threading;
 using OpenQA.Selenium;
+using System.Diagnostics;
 
 namespace POS_Automation
 {
@@ -32,14 +33,45 @@ namespace POS_Automation
             {
                 string DriverUrl = "http://127.0.0.1:4723";         //found by starting WinAppDriver.exe
                 string AppPath = @"C:\Program Files (x86)\Diamond Game Enterprises\MOL POS 20220222.1\POS.exe";
+                string appName = "POS";
                 string AppDriverPath = @"C:\Program Files (x86)\Windows Application Driver\WinAppDriver.exe";
 
+                /*
                 var appiumOptions = new AppiumOptions();
                 appiumOptions.AddAdditionalCapability("app", AppPath);
                 appiumOptions.AddAdditionalCapability("deviceName", "WindowsPC");
 
                 driver = new WindowsDriver<WindowsElement>(new Uri(DriverUrl), appiumOptions);
+                */
 
+                //start app
+                var proc = Process.Start(AppPath);
+                while (string.IsNullOrEmpty(proc.MainWindowTitle))
+                {
+                    //wait for window to pop up
+                    System.Threading.Thread.Sleep(100);
+                    proc.Refresh();
+                }
+
+                //start appium session of entire desktop
+                var desktopCapabilities = new AppiumOptions();
+                desktopCapabilities.AddAdditionalCapability("platformName", "Windows");
+                desktopCapabilities.AddAdditionalCapability("app", "Root");
+                desktopCapabilities.AddAdditionalCapability("deviceName", "WindowsPC");
+                var desktopSessions = new WindowsDriver<WindowsElement>(new Uri(DriverUrl), desktopCapabilities);
+
+                //find window of application to test and click the window into focus
+                var appWindow = desktopSessions.FindElementByName(appName);
+                appWindow.Click();
+                var appHandle = appWindow.GetAttribute("NativeWindowHandle");
+                appHandle = (int.Parse(appHandle)).ToString("x");
+
+                //start new session for target window
+                var appOptions = new AppiumOptions();
+                appOptions.AddAdditionalCapability("appTopLevelWindow", appHandle);
+                var appSession = new WindowsDriver<WindowsElement>(new Uri(DriverUrl), appOptions);
+
+                driver = appSession;
             }
 
         }
